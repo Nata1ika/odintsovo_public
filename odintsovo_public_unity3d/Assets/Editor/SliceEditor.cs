ed598a7c82c1e9558134287c3725f9c4a8e0c6a0
Natalya Gulyaeva <ngulyaeva@art3d.ru>
2017-10-11 18:56:33 +0300

    родители для модели

---
+++ odintsovo_public_unity3d/Assets/Editor/SliceEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SliceEditor : MonoBehaviour
+{
+	[MenuItem("Tools/SetSliceController")]
+	static void SetSliceController()
+	{
+		string build = "A";
+		GameObject.Find("Slice_Controller_" + build);
+	}
+}
