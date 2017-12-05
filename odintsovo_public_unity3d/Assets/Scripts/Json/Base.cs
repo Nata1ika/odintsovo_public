61fe5bedf63618e7569545f4822d7009e3e2f3fd
Natalya Gulyaeva <ngulyaeva@art3d.ru>
2017-12-05 16:15:05 +0300

    ui среза, внешнее выделение квартир, правильный текст квартир, ссылки на текст для каждого румс

---
+++ odintsovo_public_unity3d/Assets/Scripts/Json/Base.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    public class Apartament
+    {
+        public int      id;             //порядковый номер
+        public int      number;         //настоящий номер
+        public int      room;           //количество комнат
+        public int      floor;          //этаж
+        public int      section;        //секция
+        public float    square;         //площадь
+        public float    mprice;         //цена за кв. м.
+        public int      price;          //цена
+        public string   seller;         //продавец
+        public string   house;          //корпус
+        public bool     isSale;         //в продаже
+        public bool     isFavorite = false;
+        public int      groupFloor;     //группа этажей со схожими планировками
+        public int      numberFloor;    //номер на лестничной площадке
+    }
+}
